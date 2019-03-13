fa0adb1a2cfaf289f6718a5da65f0691dd3bbe69
theCodeFox <akkitta@live.co.uk>
2019-03-13 16:17:31 +0000

    default chart bug fixed

--- star-search/src/App.js
+++ star-search/src/App.js
@@ -10,7 +10,7 @@ import MeteoriteMap from './components/Map';
 class App extends Component {
   state = {
     meteoriteData: [],
-    chartType: 'map'
+    chartType: 'bubble'
   }
   render() {
     const { meteoriteData } = this.state;
